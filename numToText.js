200086915e2fb594046468d82e8bfb4438a340d5
kanimuni <kani_munidasa@vielabs.com>
2016-03-15 11:52:47 -0700

    will take a number from a string and replace it with its equalant text

--- numToText.js
+++ numToText.js
@@ -14,5 +14,39 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 */
 
 var numToText = function(str) {
+  var result = "";
+
+  var codeObj = {
+  	0:"zero",
+  	1:"one",
+  	2:"two",
+  	3:"three",
+  	4:"four",
+  	5:"five",
+  	6:"six",
+  	7:"seven",
+  	8:"eight",
+  	9:"nine"
+  }
+
+  var innerfunction = function(string) {
+  	if(string.length === 0) {
+  		return result;
+  	} else {
+  		var currentChar = string.charAt(0);
+  		if(codeObj.hasOwnProperty(currentChar)){
+  			result += codeObj[currentChar];
+  		} else {
+  			result += currentChar;
+  		}
+  	return innerfunction(string.slice(1));
+  	}
+  };
+
+  innerfunction(str);
+  console.log(result);
+  return result;
 
 };
+
+numToText("I have 5 dogs and 6 ponies");
